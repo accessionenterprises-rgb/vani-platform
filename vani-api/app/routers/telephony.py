921c089089a2f4d65b57aecc5e08521997620ca9
Accession Enterprises <accessionenterprises@gmail.com>
2026-03-28 17:46:40 +0530

    fix: increase SIP dial timeout to 120s for cold start

--- vani-api/app/routers/telephony.py
+++ vani-api/app/routers/telephony.py
@@ -29,7 +29,7 @@ async def twiml_webhook(
 
     twiml = f"""<?xml version="1.0" encoding="UTF-8"?>
 <Response>
-  <Dial timeout="30">
+  <Dial timeout="120">
     <Sip>sip:{sip_number}@{SIP_HOST};transport=tcp</Sip>
   </Dial>
 </Response>"""
